e61d11be447d001585dab8d44d8369f237a52217
unknown <davi.cesarbt@gmail.com>
2023-03-16 16:34:06 -0300

    planets distance exercise created

---
+++ planets_distance.py
@@ -0,0 +1,10 @@
+# the planets distance from the Sun
+
+earth = 149597870   # 149,597,870 km
+jupiter = 778547200 # 778,547,200 km
+
+# Objective : discover the distance between Earth and Jupiter
+
+distanceEarthAndJupiter = jupiter - earth
+
+print("The distance between Earth and Jupiter is " + f"{distanceEarthAndJupiter:,}" + "km") # result : The distance between Earth and Jupiter is 628,949,330km
