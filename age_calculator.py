33906176e7620f71c4855c60056160469cb379c6
unknown <davi.cesarbt@gmail.com>
2023-03-16 16:11:21 -0300

    files created

---
+++ age_calculator.py
@@ -0,0 +1,8 @@
+born = 2006
+actualYear = 2023
+
+def ageCalculation(bornYear, actualYear):
+    age = actualYear - born
+    return str(age)
+
+print('I am ' + ageCalculation(2006, 2023) + ' years old.')
