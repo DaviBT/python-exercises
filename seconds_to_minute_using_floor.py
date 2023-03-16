39cf4294775bf23705e5e4d6b266536f1dbdd8ef
unknown <davi.cesarbt@gmail.com>
2023-03-16 17:03:30 -0300

    minutes exercise

---
+++ seconds_to_minute_using_floor.py
@@ -0,0 +1,9 @@
+# objective : round down the result of the seconds to minute convertion
+
+seconds = 1042
+minutesRoundDown = seconds // 60
+
+seconds = str(seconds)
+minutesRoundDown = str(minutesRoundDown)
+
+print(seconds + " seconds is equivalent to " + minutesRoundDown + " minutes")
