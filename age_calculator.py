c859161059c5441570cf061806ad3dc140d8b258
Davi Torres <100081569+DaviBT@users.noreply.github.com>
2023-03-17 11:25:46 -0300

    variable born changed to birth

--- age_calculator.py
+++ age_calculator.py
@@ -1,8 +1,8 @@
-born = 2006
+birth = 2006
 actualYear = 2023
 
-def ageCalculation(bornYear, actualYear):
-    age = actualYear - born
+def ageCalculation(birthYear, actualYear):
+    age = actualYear - birth
     return str(age)
 
 print('I am ' + ageCalculation(2006, 2023) + ' years old.')
